8dd69529ef83a3d45c2f5d6759db1c3e7d98b9a5
ArskyCache <85579771+A1sky@users.noreply.github.com>
2021-07-01 18:35:08 -0400

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,13 @@
+local Data = {
+    ["Color"] = "dark" --[[Themes: dark or light or blue]],
+    ["Http Spy"] = {
+        ["Enabled"] = false,
+        ["HttpGet"] = true,
+        ["HttpPost"] = true,
+        ["HttpGetAsync"] = true,
+        ["HttpPostAsync"] = true,
+        ["Synapse Request"] = true
+    }
+}
+
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/A1sky/Kenta/main/Main.lua"), true))()
